45a4af1c4a23d87d2fb07701a6bdd1274b8a5813
fandre-b <filipe.antunes@gmail.com>
2024-03-25 12:59:29 +0000

    a

--- Versions_final/push_swap/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-make
-ARG=$(shuf -i 1-500)
-#ARG=$("1 2 4 6" "3123 23 13")
-#./push_swap $(ARG) > commnand.txt
-#./checker $(ARG) < commnand.txt
-#ARG=shuf -i 1-500 -n 500; 
-./push_swap $ARG | ./checker $ARG
